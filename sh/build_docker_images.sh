7e940472a400383230b59a493750f02d58a12c28
Jon Jagger <jon@jaggersoft.com>
2018-04-29 17:44:54 +0100

    refactoring; bash quote style

--- sh/build_docker_images.sh
+++ sh/build_docker_images.sh
@@ -2,5 +2,9 @@
 set -e
 
 readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
-cd ${ROOT_DIR}
-docker-compose --file ${ROOT_DIR}/docker-compose.yml build
+
+#cd ${ROOT_DIR}
+
+docker-compose \
+  --file "${ROOT_DIR}/docker-compose.yml" \
+  build
--- sh/test.sh
+++ sh/test.sh
@@ -3,4 +3,4 @@ set -e
 
 readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 
-ruby ${ROOT_DIR}/runner_volume_collector_test.rb
+ruby "${ROOT_DIR}/runner_volume_collector_test.rb"
